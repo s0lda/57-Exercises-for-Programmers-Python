e5e3bb34f48508fa05d6c3dfd30ba7ed1c94a5b1
Jakub <soldzik@gmail.com>
2021-07-19 00:07:28 +0100

    Create Exercise 4.py

---
+++ Exercise 4.py
@@ -0,0 +1,10 @@
+def madlib():
+    noun = str(input('Enter a noun: '))
+    verb = str(input('Enter a verb: '))
+    adjective = str(input('Enter an adjective: '))
+    adverb = str(input('Enter an adverb: '))
+    print(f'Do you {verb} your {adjective} {noun} {adverb}? That\'s hilarious!')
+
+
+if __name__ == '__main__':
+    madlib()
