11bf4a4d8101b4ba04f94caacb4bd596325fa4d2
Jakub <soldzik@gmail.com>
2021-07-25 01:37:53 +0100

    Create Exercise 14.py

---
+++ Exercise 14.py
@@ -0,0 +1,46 @@
+def user_input():
+    valid = False
+    while valid == False:
+        try:
+            number = float(input('>> '))
+            if number > 0:
+                valid = True
+                return number
+            else:
+                print('Need to be a positive number.')
+        except ValueError:
+            print('You need to input a number.')
+
+def check_state():
+    valid = False
+    while valid == False:
+        try:
+            state = input('>> ')
+            if not state.isalpha():
+                print('States don\'t contain numbers.')
+            else:    
+                valid = True
+                return state
+        finally:
+            pass
+
+def tax_calculator():
+    print('What is the order amount?')
+    order = user_input()
+    print('What is the state?')
+    state = check_state()
+    if state.upper() == "WI" or state.capitalize() == 'Wisconsin':
+        # 5.5 % tax is exercise requirement
+        tax = (order / 100) * 5.5
+        total = order + tax
+        print(f'''
+The subtotal is ${order}
+The tax is ${tax}
+The total is ${total}''')
+    else:
+        print(f'''
+The subtotal is ${order}
+The total is ${order}''')
+
+if __name__ == "__main__":
+    tax_calculator()
