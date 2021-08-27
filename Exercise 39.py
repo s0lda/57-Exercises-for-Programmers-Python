a602576e11cfed39b4f7c131d402eb14e14d51cb
Jakub <soldzik@gmail.com>
2021-08-27 23:11:11 +0100

    Update Exercise 39.py

--- Exercise 39.py
+++ Exercise 39.py
@@ -12,7 +12,7 @@ class Employee():
 emp1 = Employee('Jake', 'Jacobson', 'Programmer', 'n/a')
 emp2 = Employee('John', 'Johnson', 'Manager', '2016-12-31')
 emp3 = Employee('Michaela', 'Michaelson', 'District Manager', '2015-12-18')
-emp4 = Employee('Tou', 'Xiong', 'Software Engineer', '2016-10-05')
+emp4 = Employee('Tou', 'Xiong', 'Software Engineer', '2017-10-05')
 emp5 = Employee('Jacquelyn', 'Jackson', 'DBA', 'n/a')
 emp6 = Employee('Sally', 'Webber', 'Web Developer', '2015-12-18')
 
@@ -29,12 +29,9 @@ def sort_by_name(list_of_emps):
     for name in sorted_emps:
         for emp in list_of_emps:
             if name == emp.fullname():
-                pr_name = emp.fullname()
-                pr_position = emp.position
-                pr_separation = emp.separation
-                fullname = pr_name.ljust(25, ' ')
-                position = pr_position.ljust(20, ' ')
-                separation = pr_separation.ljust(10, ' ')
+                fullname = emp.fullname().ljust(25, ' ')
+                position = emp.position.ljust(20, ' ')
+                separation = emp.separation.ljust(20, ' ')
                 print(f'{fullname}{position}{separation}')
 
 
@@ -48,17 +45,31 @@ def sort_by_position(list_of_emps):
     for position in sorted_emps:
         for emp in list_of_emps:
             if position == emp.position:
-                pr_name = emp.fullname()
-                pr_position = emp.position
-                pr_separation = emp.separation
-                fullname = pr_name.ljust(25, ' ')
-                position = pr_position.ljust(20, ' ')
-                separation = pr_separation.ljust(10, ' ')
-                print(f'{fullname}{position}{separation}') 
+                fullname = emp.fullname().ljust(25, ' ')
+                position = emp.position.ljust(20, ' ')
+                separation = emp.separation.ljust(20, ' ')
+                print(f'{fullname}{position}{separation}')
+
+
+def sort_by_separation(list_of_emps):
+    print('\nNAME                     POSITION            SEPARATION\n')
+    sorted_emps = []
+    for emp in list_of_emps:
+        separation = emp.separation
+        if separation not in sorted_emps:
+            sorted_emps.append(separation)
+    sorted_emps = sorted(sorted_emps)
+    for sep in sorted_emps:
+        for emp in list_of_emps:
+            if sep == emp.separation:
+                fullname = emp.fullname().ljust(25, ' ')
+                position = emp.position.ljust(20, ' ')
+                separation = emp.separation.ljust(20, ' ')
+                print(f'{fullname}{position}{separation}')
 
 
 def run_the_program():
-    print('Do you want to sort by (N)ame or (P)osition? Type exit to exit.')
+    print('Do you want to sort by (N)ame, (P)osition or (S)eparation? Type exit to exit.')
     valid = False
     while valid == False:
         try:
@@ -67,6 +78,8 @@ def run_the_program():
                 sort_by_name(employees)
             elif choice.lower() == 'p':
                 sort_by_position(employees)
+            elif choice.lower() == 's':
+                sort_by_separation(employees)
             elif choice.lower() == 'exit':
                 valid = True
             else:
