21f6fdf68cce45c8962e0b6398b9b43c3866db37
Jakub <soldzik@gmail.com>
2021-09-05 00:19:46 +0100

    ex 46

---
+++ Exercise 46.py
@@ -0,0 +1,23 @@
+def file_word_counter(file_directory):
+    word_count = {'word': 0}
+
+    with open(file_directory, 'r') as f:
+        file = f.read()
+        for word in file.split():
+            for key, value in list(word_count.items()):
+                count = file.count(word)
+                up = {word: count}
+                word_count.update(up)
+
+
+    sorted_word_count = dict(sorted(word_count.items(), key= lambda item: item[1], reverse= True))
+
+
+    for key, value in sorted_word_count.items():
+        if value > 0:
+            star = value * '*'
+            print(f'{key}: {star}')
+
+
+if __name__ == '__main__':
+    file_word_counter('ex46.txt')
